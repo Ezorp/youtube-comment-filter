cb6cbe4fb3ad47717b6aef54f97eb0cd71aa51d3
elouan.crenn <elouan.crenn@epita.fr>
2025-04-24 11:48:53 +0800

    base implementation of sqlite DB

--- src/filters/database.py
+++ src/filters/database.py
@@ -1,12 +1,29 @@
 import sqlite3
 
-bandb = sqlite3.connect(".cache/bannedusers.db")
-cur = bandb.cursor()
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INTEGER
-)
-""")
+def isBanned(username, cur):
+    cur.execute("SELECT COUNT(*) FROM users WHERE name = ?",
+                (username,)
+                )
+    return cur.fetchone()[0] != 0
+
+def ban(username, cur):
+    if isBanned(username, cur):
+        return False
+    cur.execute("INSERT INTO users (name) VALUES (?)", 
+                (username))
+    cur.commit()
+    return True
+
+if __name__ == "__main__":
+    bandb = sqlite3.connect(".cache/bannedusers.db")
+    cur = bandb.cursor()
+    
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+    )
+    """)
+    cur.commit()
+    cur.close()
