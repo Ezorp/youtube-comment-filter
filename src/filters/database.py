74e342147af7599618a57143fb03bb370aa7ebca
elouan.crenn <elouan.crenn@epita.fr>
2025-04-24 11:54:51 +0800

    added banword database

--- src/filters/database.py
+++ src/filters/database.py
@@ -15,6 +15,11 @@ def ban(username, cur):
     cur.commit()
     return True
 
+def getBanwords(cur):
+    cur.execute("SELECT word FROM words")
+    word_list = [row[0] for row in cur.fetchall()]
+    return word_list
+
 if __name__ == "__main__":
     bandb = sqlite3.connect(".cache/bannedusers.db")
     cur = bandb.cursor()
@@ -25,5 +30,18 @@ if __name__ == "__main__":
         name TEXT NOT NULL
     )
     """)
+
+    banworddb = sqlite3.connect(".cache/banwords.db")
+    cur2 = bandb.cursor()
+    
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS words (
+        id INTEGER PRIMARY KEY,
+        word TEXT NOT NULL
+    )
+    """)
+
     cur.commit()
     cur.close()
+    cur2.commit()
+    cur2.close()
